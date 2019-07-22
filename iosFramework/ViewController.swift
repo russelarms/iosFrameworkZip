3b2388a8a26ef43afc29b8d1113c83986366308d
Ruslan Urmeev <v-ruurme@microsoft.com>
2019-07-22 15:18:50 +0400

    initial commit

---
+++ iosFramework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosFramework
+//
+//  Created by Руслан Урмеев on 22/07/2019.
+//  Copyright © 2019 appsupport. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
